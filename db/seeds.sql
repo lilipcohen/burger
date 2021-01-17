c69b8d0af699453b7b378fc85233e77b020d11c4
Lili Cohen <lili.cohen.tv@gmail.com>
2021-01-17 14:54:25 -0800

    adding sql files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Runny Outta Thyme Burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Peas and Thank You Burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sweet Home Avocado Burger', false);
