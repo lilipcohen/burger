b11a4140bcfc321501eb5d2b6361d88fa198f7fc
Lili Cohen <lili.cohen.tv@gmail.com>
2021-01-19 15:02:53 -0800

    adding index linking to each file

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,13 @@
 const connection = require("./connection.js");
 
 var orm = {
-    selectAll: function () {
-        const queryString = "";
-        connection.query(queryString, [whatToSelect, tableInput], function(err, result) {
-            if (err) throw err;
-            console.log(result);
+    selectAll: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+            throw err;
+        }
+        cb(result);
         });
     },
     insertOne: function () {
