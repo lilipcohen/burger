fdf61a43ccc6e9adb18c685469539698f585d402
Lili Cohen <lili.cohen.tv@gmail.com>
2021-01-20 20:38:05 -0800

    adding jquery and function to script

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,21 +1,23 @@
-$(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
+$(function() {
+    $(".create-form").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
 
-    var newBurger = {
-      burger_name: $("#burgerForm").val().trim(),
-      devoured: $("[name=sleepy]:checked").val().trim()
-    };
+        var newBurger = {
+            burger_name: $("#burgerForm").val().trim(),
+            devoured: $("[name=sleepy]:checked").val().trim()
+        };
 
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+})
