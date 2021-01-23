ea8a00c73f2e6c88bd899278be454f6047d38683
Lili Cohen <lili.cohen.tv@gmail.com>
2021-01-22 16:00:46 -0800

    adding fixes to update and create orms

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 module.exports = router;
-
+ 
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
     var hbsObject = {
@@ -18,11 +18,10 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.insertOne([
-    "burger_name", "devoured"
+    "burger_name"
   ], [
-    req.body.burger_name, req.body.devoured
+    req.body.burger_name
   ], function(result) {
-    // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
@@ -30,13 +29,10 @@ router.post("/api/burgers", function(req, res) {
 router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
-
   burger.updateOne({
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
       res.status(200).end();
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,5 @@
 $(function() {
     $(".create-form").on("submit", function (event) {
-        // Make sure to preventDefault on a submit event.
         event.preventDefault();
 
         const newBurger = {
@@ -8,35 +7,29 @@ $(function() {
             devoured: false
         };
 
-        // Send the POST request.
         $.ajax("/api/burgers", {
             type: "POST",
             data: newBurger
         }).then(
             function () {
                 console.log("created new burger");
-                // Reload the page to get the updated list
                 location.reload();
             }
         );
     });
 
     $("#change-devoured").on("click", function(event) {
-    const id = $(this).data("burger_name");
-    const newDevoured = $(this).data("newdevoured");
+    const id = $(this).data("id");
 
     const newDevouredBurger = {
       devoured: true
     };
 
-    // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newDevouredBurger
     }).then(
       function() {
-        console.log("changed devoured to", newDevoured);
-        // Reload the page to get the updated list
         location.reload();
       }
     );
